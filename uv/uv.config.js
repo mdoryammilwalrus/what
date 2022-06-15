6717f60ea014aeca593e2ceef67d30fddfa09f21
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-06-14 23:07:13 -0500

    please work uv

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/playground/_/',
+    bare: 'https://client.incog.dev/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/playground/uv/uv.handler.js',
+    bundle: '/playground/uv/uv.bundle.js',
+    config: '/playground/uv/uv.config.js',
+    sw: '/playground/uv/uv.sw.js',
+};
