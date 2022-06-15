f4cb7516fdf59b6df9f7bb6be0decbd5e16f0a6f
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-06-14 23:22:29 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/confusion/_/',
+    prefix: '/playground/_/',
     bare: 'https://client.incog.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/confusion/uv/uv.handler.js',
-    bundle: '/confusion/uv/uv.bundle.js',
-    config: '/confusion/uv/uv.config.js',
-    sw: '/confusion/uv/uv.sw.js',
+    handler: '/playground/uv/uv.handler.js',
+    bundle: '/playground/uv/uv.bundle.js',
+    config: '/playground/uv/uv.config.js',
+    sw: '/playground/uv/uv.sw.js',
 };
