d8f6ea4d46b7800e36dd27e857650712ce328b32
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-08-17 23:55:47 -0500

    .

---
+++ internal/manager.js
@@ -0,0 +1,8 @@
+const internal_pages = {
+    // YOU MUST ADD YOUR INTERNAL PAGE TO THIS FILE IF YOU WANT IT TO WORK!
+    // This index is how HT can reference what page to what file
+    newtab: "/playground/internal/newTab/main.html",
+    extensionsmarketplace: "/playground/internal/extensions/marketplace.html",
+    extensions: "/playground/internal/extensions/index.html",
+    games: "/playground/internal/g/index.html"
+}
